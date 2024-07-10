fad477e9449f71f9674fbb10ac919ea32cc7cf27
Mathieu52 <Mathieu52@users.noreply.github.com>
2024-07-10 22:22:58 +0000

    deploy: b7167d3d8b5a0c3c40e88012e329c4374017a158

--- search/all_5.js
+++ search/all_5.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['setencoderconversionratio_0',['setEncoderConversionRatio',['../classMotorControl.html#a87ffc6ce32b3737c5883b922f8e85ad0',1,'MotorControl']]],
-  ['setencodertickperturn_1',['setEncoderTickPerTurn',['../classMotorControl.html#af20ccec84bdceb13b0c2577f22c2f996',1,'MotorControl']]],
-  ['setgearboxratio_2',['setGearBoxRatio',['../classMotorControl.html#ac61b3851b77068a037b667793da0bfef',1,'MotorControl']]],
-  ['setmotorid_3',['setMotorID',['../classMotorControl.html#ad1c151e663c6ed794921128bccf08be8',1,'MotorControl']]]
+  ['setconversionratio_0',['setConversionRatio',['../classMotorControl.html#ae51685cd826ef934d2c53cc2ffb70eb5',1,'MotorControl']]],
+  ['setencoderid_1',['setEncoderID',['../classMotorControl.html#a9dc7043322854c4a21e2110556cc9aac',1,'MotorControl']]],
+  ['setencodertickperturn_2',['setEncoderTickPerTurn',['../classMotorControl.html#af20ccec84bdceb13b0c2577f22c2f996',1,'MotorControl']]],
+  ['setgearboxratio_3',['setGearBoxRatio',['../classMotorControl.html#ac61b3851b77068a037b667793da0bfef',1,'MotorControl']]],
+  ['setmotorid_4',['setMotorID',['../classMotorControl.html#ad1c151e663c6ed794921128bccf08be8',1,'MotorControl']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['getencoderconversionratio_0',['getEncoderConversionRatio',['../classMotorControl.html#a6c36ef1a40d3c62363723503cf0d1855',1,'MotorControl']]],
-  ['getencodertickperturn_1',['getEncoderTickPerTurn',['../classMotorControl.html#a01f89ee7c23f224f09a5e3d86c82d9da',1,'MotorControl']]],
-  ['getgearboxratio_2',['getGearBoxRatio',['../classMotorControl.html#abfc226cd4543c904dc03e841fd0b4e73',1,'MotorControl']]],
-  ['getmotorid_3',['getMotorID',['../classMotorControl.html#aef32ad24b81c9834c8305e296d7b3efb',1,'MotorControl']]]
+  ['getconversionratio_0',['getConversionRatio',['../classMotorControl.html#a778d7da01680f52bc9d6b3d9cbfa60d6',1,'MotorControl']]],
+  ['getencoderid_1',['getEncoderID',['../classMotorControl.html#ad32f94230c4cc2a5130096446c7a3dbc',1,'MotorControl']]],
+  ['getencodertickperturn_2',['getEncoderTickPerTurn',['../classMotorControl.html#a01f89ee7c23f224f09a5e3d86c82d9da',1,'MotorControl']]],
+  ['getgearboxratio_3',['getGearBoxRatio',['../classMotorControl.html#abfc226cd4543c904dc03e841fd0b4e73',1,'MotorControl']]],
+  ['getmotorid_4',['getMotorID',['../classMotorControl.html#aef32ad24b81c9834c8305e296d7b3efb',1,'MotorControl']]]
 ];
