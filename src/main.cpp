1bbbc2a2eadce05cc38972fd1ebc75b20844188d
Mathieu Durand <mathieu_durand@icloud.com>
2024-07-11 08:36:44 -0400

    Prototype main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************************************
 Nom du fichier : main.cpp
 Auteur : Mathieu Durand
-Date de création : 2023/10/16
+Date de création : 2024/7/11
 
 Description : Fichier principale Arduino
               
@@ -12,50 +12,71 @@ Modifications :
 ***************************************************************************************************/
 
 // *************************************************************************************************
+//  HEADER
+// *************************************************************************************************	
+
+void loopPID();
+
+// *************************************************************************************************
 //  INCLUDES
 // *************************************************************************************************	
 
 #include <Arduino.h>
-#include <LibRobus.h>
+#include <LibS3GRO.h>
+#include "MotorController.h"
 
-// *************************************************************************************************
-//  CONSTANTES
-// *************************************************************************************************
-/* VIDE */
+#define HALF_DISTANCE 50 // TODO: Measure the half distance to travel
 
-// *************************************************************************************************
-//  FONCTIONS LOCALES
-// *************************************************************************************************
-/* VIDE */
+ArduinoX arduinoX;
 
-// *************************************************************************************************
-//  STRUCTURES ET UNIONS
-// *************************************************************************************************
-/* VIDE */
+PID positionPID;
+// TODO: Get the real tick per turn and wheel diameter
+MotorController positionMotorController(&arduinoX, 0, 0, 50, 25, MotorController::wheelRatio(5));
+double positionMeasurement() {return positionMotorController.measurementFunction();}
+void positionCommand(double command) {positionMotorController.commandFunction(command);}
 
-// *************************************************************************************************
-// VARIABLES GLOBALES
-// *************************************************************************************************
-/* VIDE */
+PID armPID;
+// TODO: Get the real tick per turn, gearbox ratio and discuss the unit of control (Turns, degrees or radians) for now degrees
+MotorController armMotorController(&arduinoX, 0, 0, 50, 25, MotorController::degreesRatio());
+double armMeasurement() {return armMotorController.measurementFunction();}
+void armCommand(double command) {armMotorController.commandFunction(command);}
 
 /**
- * @brief Initialisation du programme.
+ * @brief Program initialisation
  * @author Mathieu Durand
  */
 void setup()
 {   
-    BoardInit();
+    arduinoX.init();
+
+    positionPID.setMeasurementFunc(positionMeasurement);
+    positionPID.setCommandFunc(positionCommand);
+    positionPID.setGains(1, 0, 0); // TODO: Tune the position PID
+
+    armPID.setMeasurementFunc(armMeasurement);
+    armPID.setCommandFunc(armCommand);
+    armPID.setGains(1, 0, 0); // TODO: Tune the arm PID
+
+    positionPID.setGoal(HALF_DISTANCE);
+
     Serial.begin(115200); // 115200 ou 9600
-    
-    // Décommenter si le programme a absolument besoin du serial.
-    //while(!Serial);
 }
 
 /**
- * @brief Boucle du programme.
+ * @brief Progam main loop
  * @author Mathieu Durand
 */
 void loop()
 {
-    
+    loopPID();
+}
+
+/**
+ * @brief PID update loop
+ * @author Mathieu Durand
+*/
+void loopPID()
+{
+    positionPID.run();
+    armPID.run();
 }
